6c94fde493c6d327e0c96e2931be29d1657488ad
morz1ck <120595021+morz1ck@users.noreply.github.com>
2025-08-23 22:03:57 +0300

    Update KV-mark.lua
    Исправлена кодировка файла. Пофиксил баг с нулевым сектором

--- KV-mark.lua
+++ KV-mark.lua
@@ -1,55 +1,58 @@
--- <мик>>else'(мяк>)=*
-
 script_name('Learning')
 script_author('morz1ck')
-
+ 
 require "lib.moonloader"
 local tag = '[KV-mark]: ', 0xFF1493
-local sampev = require "lib.samp.events"
-local imgui = require "imgui"
-
-
+local sampev = require("lib.samp.events");
+local imgui = require("imgui");
+local cordX = nil;
+local cordY = nil;
+ 
 function main()
-	if not isSampLoaded() or not isSampfuncsLoaded() then return end
-	while not isSampAvailable() do wait(100) end
-
-	res, id = sampGetPlayerIdByCharHandle(PLAYER_PED)
-	nick = sampGetPlayerNickname(id)
-	sampAddChatMessage(tag .. " {FFFFFF}You are welcome {FF69B4}" .. nick, 0xFF69B4)
-	sampRegisterChatCommand("setmark", cmd_setmark)
-  sampRegisterChatCommand('hel', cmd_hello)
-
-	while true do
-		wait(0)
-  end
+    while not isSampAvailable() do wait(0) end
+ 
+    res, id = sampGetPlayerIdByCharHandle(PLAYER_PED);
+    nick = sampGetPlayerNickname(id);
+    sampAddChatMessage(tag .. " {FFFFFF}You are welcome {FF69B4}" .. nick, 0xFF1493);
+    sampRegisterChatCommand("setmark", cmd_setmark);
+    sampRegisterChatCommand('hel', cmd_hello);
+ 
+    wait(-1);
 end
-
+ 
 function cmd_setmark()
-  if cordX ~= nil and cordY ~= nil then
-    local cX, cY, cZ = getCharCoordinates(playerPed)
-    cX = math.ceil(cx)
-    cY = math.ceil(cy)
-    sampAddChatMessage(tag .. "Waypoint "..kvadY.."-"..kvadX.." is ready."..math.ceil(getDistanceBetweenCoords2d(cordX, cordY, cx, cy)) .. ' i.', -1)
-    placeWaypoint(cordX, cordY, 0)
+    if cordX ~= nil and cordY ~= nil then
+        cX, cY, cZ = getCharCoordinates(playerPed)
+        cX = math.ceil(cX)
+        cY = math.ceil(cY)
+        sampAddChatMessage(tag .. "{ffffff}Waypoint "..kvadY.."-"..kvadX.." is ready. ", 0xFF1493)
+        placeWaypoint(cordX, cordY, 0);
+    else
+      sampAddChatMessage(tag .. '{ffffff}There is no sector', 0xFF1493)
+    end
+end
+ 
+ 
+function cmd_hello(player_id)
+    player_nickname = sampGetPlayerNickname(player_id)
+    sampSendChat("Good evening, ".. player_nickname .. ".")
+end
+ 
+function sampev.onServerMessage(color, lcs)
+  local pattern = "([A-РЇР°-СЏРђ-Za-z])%-(%d%d?)"
+  local foundY, foundX = string.match(lcs, pattern)
+
+  if foundY and foundX then
+    kvadY = foundY
+    kvadX = foundX
+    local kvNum = kvadrat(kvadY)
+    if kvNum ~= nil and tonumber(kvadX) < 25 and tonumber(kvadX) > 0 then
+      cordX = tonumber(kvadX) * 250 - 3125
+      cordY = 3125 - (kvNum * 250)
+    end
   end
 end
 
-
-function cmd_hello(player_id)
-  player_nickname = sampGetPlayerNickname(player_id)
-  sampSendChat("Good evening, ".. player_nickname .. ".")
-end
-
-function sampev.onServerMessage(color, lcs)
-	if string.find(lcs, "(%A)-[0-9][0-9]") or string.find(lcs, "(%A)-[0-9]") then
-		kvadY, kvadX = string.match(lcs, '(%A)-(%d+)')
-		if kvadrat(kvadY) ~= nil and kvadX ~= nil and tonumber(kvadX) < 25 and tonumber(kvadX) > 0 then
-			last = lcs
-			cordX = kvadX * 250 - 3125
-			cordY = (kvadrat(kvadY) * 250 - 3125) * - 1
-		end
-	end
-end
 
 function kvadrat(param)
     local KV = {
@@ -60,7 +63,7 @@ function kvadrat(param)
         ["а"] = 1, ["б"] = 2, ["в"] = 3, ["г"] = 4, ["д"] = 5, ["е"] = 6, ["ж"] = 7,
         ["з"] = 8, ["и"] = 9, ["й"] = 10, ["к"] = 11, ["л"] = 12, ["м"] = 13,
         ["н"] = 14, ["о"] = 15, ["п"] = 16, ["р"] = 17, ["с"] = 18, ["т"] = 19,
-        ["у"] = 20, ["ф"] = 21, ["х"] = 22, ["ц"] = 23, ["ч"] = 24,
+        ["у"] = 20, ["ф"] = 21, ["х"] = 22, ["ц"] = 23, ["ч"] = 24
     }
     return KV[param]
 end
